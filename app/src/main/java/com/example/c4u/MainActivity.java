987a0740fffa88ecdb358e765bf8bf2816b41942
hamzatamry <58271202+hamzatamry@users.noreply.github.com>
2021-04-02 23:14:41 +0100

    Vérification des différentes capteurs présents sur mon appareil

--- app/src/main/java/com/example/c4u/MainActivity.java
+++ app/src/main/java/com/example/c4u/MainActivity.java
@@ -2,16 +2,81 @@ package com.example.c4u;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.hardware.*;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class MainActivity extends AppCompatActivity
+{
+    private static SensorManager sensorManager = null;
+
+    private void checkSensorList()
+    {
+        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        Iterator<Sensor> listIterator = deviceSensors.iterator();
+
+        while (listIterator.hasNext())
+        {
+            Sensor sensor = listIterator.next();
+            System.out.println(sensor);
+        }
+    }
+
+    private Boolean checkIfAccelerometerSensorExists()
+    {
+        return sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null;
+    }
+
+    private Boolean checkIfGravitySensorExists()
+    {
+        return sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) != null;
+    }
+
+    private Boolean checkIfGyroscopeSensorExists()
+    {
+        return sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED) != null;
+    }
+
+    private Boolean checkIfLinearAccelerationSensorExists()
+    {
+        return sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null;
+    }
+
+    private Boolean checkIfOrientationSensorExists()
+    {
+        return sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION) != null;
+    }
+
+    private Boolean checkIfRotationVectorSensorExists()
+    {
+        return sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) != null;
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("helloWor");
+
+        MainActivity.sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+
+        System.out.println("AccelerometerSensor " + checkIfAccelerometerSensorExists());
+        System.out.println("GravitySensor " + checkIfGravitySensorExists());
+        System.out.println("GyroscopeSensor " + checkIfGyroscopeSensorExists());
+        System.out.println("LinearAccelerationSensor " + checkIfLinearAccelerationSensorExists());
+        System.out.println("OrientationSensor " + checkIfOrientationSensorExists());
+        System.out.println("RotationVectorSensor " + checkIfRotationVectorSensorExists());
     }
 }
