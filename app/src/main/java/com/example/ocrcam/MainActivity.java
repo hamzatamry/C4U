6eb6a12b9f9234add0f3eaf7cb960e08c9663c21
Delwix <abdelwadoudtm@gmail.com>
2021-04-02 12:39:42 +0100

    Added homepage activity

--- app/src/main/java/com/example/ocrcam/MainActivity.java
+++ app/src/main/java/com/example/ocrcam/MainActivity.java
@@ -3,34 +3,16 @@ package com.example.ocrcam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.speech.tts.TextToSpeech;
-import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.text.TextBlock;
-import com.google.android.gms.vision.text.TextRecognizer;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imgView;
-    private Button TakePicButt;
-    private TextRecognizer recognizer;
-    private TextView textView;
-    private Button ttsButton;
-    private EditText ttsText;
-    TextToSpeech textToSpeech;
-    private static final int REQUEST_IMAGE_CAPTURE = 101;
+
+    private Button ocrBt;
+    private Button geoBt;
+    private Button moneyBt;
 
 
     @Override
@@ -38,67 +20,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgView = findViewById(R.id.imageView);
-        TakePicButt = findViewById(R.id.button);
-        textView = findViewById(R.id.textView);
-        ttsButton = findViewById(R.id.button2);
-        ttsText = findViewById(R.id.editText);
+        ocrBt = findViewById(R.id.ocrBt);
+        geoBt = findViewById(R.id.geoBt);
+        moneyBt = findViewById(R.id.moneyBt);
 
-         textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if(status != TextToSpeech.ERROR){
-                    textToSpeech.setLanguage(Locale.ENGLISH);
-                }
-            }
-        });
-        ttsButton.setOnClickListener(new View.OnClickListener() {
+        ocrBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String ttsSpeech = ttsText.getText().toString();
-                textToSpeech.speak(ttsSpeech,TextToSpeech.QUEUE_FLUSH,null);
-
+                Intent intent = new Intent(MainActivity.this, OcrActivity.class);
+                startActivity(intent);
             }
         });
 
-        TakePicButt.setOnClickListener(new View.OnClickListener() {
+        moneyBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent imgTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if(imgTakeIntent.resolveActivity(getPackageManager()) != null){
-                    startActivityForResult(imgTakeIntent,100);
-                }
+                Intent intent = new Intent(MainActivity.this, MoneyDetectActivity.class);
+                startActivity(intent);
             }
         });
-    }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        textToSpeech.shutdown();
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 100 && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imgBitmap = (Bitmap) extras.get("data");
-            imgView.setImageBitmap(imgBitmap);
-
-
-            recognizer = new TextRecognizer.Builder(MainActivity.this).build();
-            Frame frame = new Frame.Builder().setBitmap(imgBitmap).build();
-            SparseArray<TextBlock> sparseArray = recognizer.detect(frame);
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i<sparseArray.size(); i++){
-                TextBlock tb = sparseArray.get(i);
-                String res = tb.getValue();
-                stringBuilder.append(res);
+        geoBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GeoActivity.class);
+                startActivity(intent);
             }
-            textView.setText(stringBuilder);
-        }
+        });
     }
 
 
---
+++ app/src/main/java/com/example/ocrcam/MoneyDetectActivity.java
@@ -0,0 +1,14 @@
+package com.example.ocrcam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoneyDetectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_money_detect);
+    }
+}
