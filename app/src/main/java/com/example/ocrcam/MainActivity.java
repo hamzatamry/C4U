3f03b3a6ef090a9d0cdea8511f9eb61f420c053a
Delwix <abdelwadoudtm@gmail.com>
2021-04-03 10:05:17 +0100

    Voice recognition added

--- app/src/main/java/com/example/ocrcam/MainActivity.java
+++ app/src/main/java/com/example/ocrcam/MainActivity.java
@@ -1,11 +1,26 @@
 package com.example.ocrcam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static android.speech.RecognizerIntent.EXTRA_LANGUAGE;
+import static android.speech.RecognizerIntent.EXTRA_LANGUAGE_MODEL;
+import static android.speech.RecognizerIntent.LANGUAGE_MODEL_FREE_FORM;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +28,11 @@ public class MainActivity extends AppCompatActivity {
     private Button ocrBt;
     private Button geoBt;
     private Button moneyBt;
-
+    private Button voiceBt;
+    private TextView textView;
+    private Intent speechIntent;
+    private SpeechRecognizer speechRec;
+    private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +42,85 @@ public class MainActivity extends AppCompatActivity {
         ocrBt = findViewById(R.id.ocrBt);
         geoBt = findViewById(R.id.geoBt);
         moneyBt = findViewById(R.id.moneyBt);
+        voiceBt = findViewById(R.id.voiceBt);
+        textView = findViewById(R.id.textView5);
+
 
         ocrBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, OcrActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_RECORD_AUDIO);
+        }
+
+        speechRec = SpeechRecognizer.createSpeechRecognizer(MainActivity.this);
+        speechRec.setRecognitionListener(new RecognitionListener() {
+
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                textView.setText("Listenning ...");
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+                textView.setText(""+error);
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                ArrayList<String> res = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                String listString = "";
+
+                for (String s : res)
+                {
+                    listString += s + " ";
+                }
+                textView.setText(listString);
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
+        speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechIntent.putExtra(EXTRA_LANGUAGE_MODEL,LANGUAGE_MODEL_FREE_FORM);
+        speechIntent.putExtra(EXTRA_LANGUAGE,"fr-FR");
+
+        voiceBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speechRec.startListening(speechIntent);
             }
         });
 
@@ -40,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         geoBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,6 +141,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
 
 }
