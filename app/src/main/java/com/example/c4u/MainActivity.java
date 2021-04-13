f1f62d2aae376eabf42a95cd9a3c0d25a5f79125
hamzatamry <58271202+hamzatamry@users.noreply.github.com>
2021-04-13 19:01:41 +0000

    Ajout de détection des positions (portrait, landscape, vertical) et du mouvement du shake ainsi que si l'environement est sombre ou lumineux

--- app/src/main/java/com/example/c4u/MainActivity.java
+++ app/src/main/java/com/example/c4u/MainActivity.java
@@ -6,9 +6,12 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.hardware.*;
+import android.os.CountDownTimer;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -22,6 +25,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Locale;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener
@@ -33,11 +37,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView orientationXTextView = null;
     private TextView orientationYTextView = null;
     private TextView orientationZTextView = null;
-    private TextView proximityTextView = null;
     private TextView lightTextView = null;
-    private TextView geomagneticRotationXTextView = null;
-    private TextView geomagneticRotationYTextView = null;
-    private TextView geomagneticRotationZTextView = null;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+    private TextToSpeech ttobj = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -51,18 +57,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         orientationXTextView = findViewById(R.id.orientationX);
         orientationYTextView = findViewById(R.id.orientationY);
         orientationZTextView = findViewById(R.id.orientationZ);
-        proximityTextView = findViewById(R.id.proximity);
         lightTextView = findViewById(R.id.light);
-        geomagneticRotationXTextView = findViewById(R.id.geomagneticRotationX);
-        geomagneticRotationYTextView = findViewById(R.id.geomagneticRotationY);
-        geomagneticRotationZTextView = findViewById(R.id.geomagneticRotationZ);
-
         this.sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         registerSensorEventListener(Sensor.TYPE_ACCELEROMETER);
         registerSensorEventListener(Sensor.TYPE_ORIENTATION);
-        registerSensorEventListener(Sensor.TYPE_PROXIMITY);
         registerSensorEventListener(Sensor.TYPE_LIGHT);
-        registerSensorEventListener(Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR);
+
+        mAccel = 10f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+
+
+
     }
 
     @Override
@@ -97,22 +103,107 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 accelerationXTextView.setText("Acceleration x-axis: " + event.values[0]);
                 accelerationYTextView.setText("Acceleration y-axis: " + event.values[1]);
                 accelerationZTextView.setText("Acceleration z-axis: " + event.values[2]);
+
+                //Detect shake
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+                mAccelLast = mAccelCurrent;
+                mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+                float delta = mAccelCurrent - mAccelLast;
+                mAccel = mAccel * 0.9f + delta;
+                if (mAccel > 12) {
+
+                    ttobj = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                        @Override
+                        public void onInit(int status) {
+                            if (status != TextToSpeech.ERROR){
+                                ttobj.setLanguage(Locale.UK);
+                            }
+
+                            ttobj.speak("Shake event detected", TextToSpeech.QUEUE_FLUSH, null);
+                        }
+                    });
+
+
+                    Toast.makeText(getApplicationContext(), "Shake event detected", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case Sensor.TYPE_ORIENTATION:
+
+                float xOrientation = event.values[1];
+                float yOrientation = event.values[2];
+                Log.i("xOrientation: ", String.valueOf(xOrientation));
+                Log.i("yOrientation: ", String.valueOf(yOrientation));
+                String text = "";
+
+                if (-90 <= xOrientation && xOrientation <= -70)
+                {
+                    text = "Portrait position";
+                }
+
+                if (-20 <= xOrientation && xOrientation <= 1)
+                {
+                    text = "Vertical position";
+                }
+
+                if (70 <= yOrientation && yOrientation <= 90 || -90 <= yOrientation && yOrientation <= -70)
+                {
+                    text = "Landscape position";
+
+                }
+
                 orientationXTextView.setText("Orientation x-axis: " + event.values[1]);
                 orientationYTextView.setText("Orientation y-axis: " + event.values[2]);
                 orientationZTextView.setText("Orientation z-axis: " + event.values[0]);
-                break;
-            case Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR:
-                geomagneticRotationXTextView.setText("Geomagnetic Rotation x-axis: " + event.values[0]);
-                geomagneticRotationYTextView.setText("Geomagnetic Rotation y-axis: " + event.values[1]);
-                geomagneticRotationZTextView.setText("Geomagnetic Rotation z-axis: " + event.values[2]);
-                break;
-            case Sensor.TYPE_PROXIMITY:
-                proximityTextView.setText("Proximity: " + event.values[0]);
+
+                if (text != "")
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+
+                    CountDownTimer toastCountDown;
+                    toastCountDown = new CountDownTimer(1, 1) {
+                        public void onTick(long millisUntilFinished) {
+                            toast.show();
+
+
+                        }
+                        public void onFinish() {
+                            toast.cancel();
+                        }
+                    };
+                    toast.show();
+                    toastCountDown.start();
+                }
+
                 break;
             case Sensor.TYPE_LIGHT:
+                if (event.values[0] <= 1)
+                {
+                    Toast mToastToShow = Toast.makeText(this, "No Light Detected", Toast.LENGTH_SHORT);
+
+                    CountDownTimer toastCountDown;
+                    toastCountDown = new CountDownTimer(1, 1) {
+                        public void onTick(long millisUntilFinished) {
+                            mToastToShow.show();
+
+
+                        }
+                        public void onFinish() {
+                            mToastToShow.cancel();
+                        }
+                    };
+
+                    // Show the toast and starts the countdown
+                    mToastToShow.show();
+                    toastCountDown.start();
+
+
+                }
                 lightTextView.setText("Illuminance: " + event.values[0]);
+
+
+
             default:
                 break;
         }
