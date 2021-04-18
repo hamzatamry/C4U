9f3b20d2232ab8e0105439f98fe6db28267d02c2
ZakariaSabour <zakaria.sabour99@gmail.com>
2021-04-18 22:47:30 +0000

    google maps api now working

--- app/src/main/java/com/example/c4u/MapsActivity.java
+++ app/src/main/java/com/example/c4u/MapsActivity.java
@@ -1,119 +1,146 @@
 package com.example.c4u;
 
 import androidx.annotation.NonNull;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
+
 
 import android.Manifest;
-import android.content.Context;
+
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+
 import android.os.Bundle;
 
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-    LocationManager locationManager;
-    LocationListener locationListener;
-    LatLng userLatLong;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-    private GoogleMap mMap;
+
+public class MapsActivity extends AppCompatActivity {
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
+    private Geocoder geocoder;
+    TextToSpeech tts;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        // initialize fused location
+        client = LocationServices.getFusedLocationProviderClient(this);
+        geocoder = new Geocoder(this);
+
+        // check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // if granted
+            getCurrentLocation();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.US);
+                }
+            }
+        });
+    }
+
+    private void getCurrentLocation() {
+        // initialize task location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // when succes
+                if( location != null){
+                    //syncMap
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+                            List<Address> addresses = null;
+                            try {
+                                addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                                if (addresses.size() > 0)
+                                {
+                                    Address address = addresses.get(0);
+                                    String locationName = address.getCountryName();
+                                    Log.d("location",locationName);
+                                    Toast.makeText(MapsActivity.this, locationName, Toast.LENGTH_SHORT).show();
+                                    tts.speak(locationName, TextToSpeech.QUEUE_ADD, null);
+                                }
+                                else{
+                                    Toast.makeText(MapsActivity.this, "endroit inconnu", Toast.LENGTH_SHORT).show();
+                                    tts.speak("unknown location", TextToSpeech.QUEUE_FLUSH, null);
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            // create marker options
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("you are here");
+
+                            //zoom
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+
+                            //add Marker on map
+                            googleMap.addMarker(options);
+
+                            Log.d("msg", "********************************");
+                        }
+                    });
+
+                }
+            }
+        });
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(@NonNull Location location) {
-                userLatLong = new LatLng(location.getLatitude(), location.getLongitude());
-                mMap.clear();
-                mMap.addMarker(new MarkerOptions().position(userLatLong).title("You are Here"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(userLatLong));
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 44){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                // when permission granted
+                getCurrentLocation();
             }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(@NonNull String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(@NonNull String provider) {
-
-            }
-        };
-
-        //location permission
-        askLocationPermission();
-
-        // Add a marker in Sydney and move the camera
-        //LatLng sydney = new LatLng(-34, 151);
-        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
-    private void askLocationPermission() {
-        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
-            @Override
-            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    return;
-                }
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
-                // set the default location Marker to last known position
-                //Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                Location lastLocation = new Location(LocationManager.GPS_PROVIDER);
-                userLatLong = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
-                mMap.clear();
-                mMap.addMarker(new MarkerOptions().position(userLatLong).title("Your are Here"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(userLatLong));
-
-                //LatLng casablanca = new LatLng(-34, 151);
-                //mMap.addMarker(new MarkerOptions().position(casablanca).title("Marker in Sydney"));
-                //mMap.moveCamera(CameraUpdateFactory.newLatLng(casablanca));
-
-
-            }
-
-            @Override
-            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-
-            }
-
-            @Override
-            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-                permissionToken.continuePermissionRequest();
-            }
-        }).check();
+        }
     }
 }
+
