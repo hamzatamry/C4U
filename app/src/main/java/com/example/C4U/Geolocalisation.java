52315045e049af7a2d604fe128a56b2beee4dde4
ZakariaSabour <zakaria.sabour99@gmail.com>
2021-06-01 21:53:09 +0100

    removed some comments

--- app/src/main/java/com/example/C4U/Geolocalisation.java
+++ app/src/main/java/com/example/C4U/Geolocalisation.java
@@ -55,12 +55,8 @@ public class Geolocalisation extends AppCompatActivity {
 
     private void getCurrentLocation() {
         // initialize task location
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         Task<Location> task = client.getLastLocation();
